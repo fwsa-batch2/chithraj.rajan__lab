7afb504217bb566b27c371d2fe01f7f3f1d7b977
Chithraj <chithraj23102003@gmail.com>
2022-05-19 18:08:03 +0530

    added some files

---
+++ JAVA/If loop/IfLoop.java
@@ -0,0 +1,20 @@
+public class IfLoop {
+    public static void main(String[] args) {
+
+        int run = 50 ;
+
+        if (run<50){
+            System.out.println("Good Player");
+        }
+        else if (run == 50) {
+            System.out.println("Half century");
+        }
+        else if (run > 50) {
+            System.out.println("Century is near...Keep it up");
+        }
+        else {
+            System.out.println("Fire is on");
+        }
+
+    }
+}
