17141563b79a7056b5034e0b71a19121d28ec1e2
Chithraj <chithraj23102003@gmail.com>
2022-05-20 14:24:15 +0530

    added some files

--- Java practices/src/FWSA/IfLoop.java
+++ Java practices/src/FWSA/IfLoop.java
@@ -1,3 +1,5 @@
+package FWSA;
+
 public class IfLoop {
     public static void main(String[] args) {
 
@@ -14,6 +16,23 @@ public class IfLoop {
         }
         else {
             System.out.println("Fire is on");
+        }
+
+        int a = 1;
+        switch (a){
+            case 0:
+                System.out.println("it is zero");
+                break;
+            case 1:
+                System.out.println("it is one");
+                break;
+            case 2:
+                System.out.println("it is two");
+                break;
+            default:
+                System.out.println("please enter any number");
+                break;
+
         }
 
     }
