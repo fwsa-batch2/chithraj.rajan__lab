783b2941a05ce67b61bedc924cf09ccdb9067697
Chithraj <chithraj23102003@gmail.com>
2022-05-24 14:28:36 +0530

    added some files

---
+++ Java practices/src/FWSA/Tables.java
@@ -0,0 +1,28 @@
+package FWSA;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Tables {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        if (2<=N && 20>=N){
+
+            for(int i = 1; i<=10; i++ ){
+                System.out.println(N + "x" + i + "=" + N*i);
+
+            }
+        }
+    }
+    }
+
