720760011c40e81df54e1f1191f3c2d82cde3e0a
Chithraj <chithraj23102003@gmail.com>
2022-06-05 10:52:33 +0530

    added some files

--- Java practices/src/FWSA/forloop.java
+++ Java practices/src/FWSA/forloop.java
@@ -1,4 +1,6 @@
 package FWSA;
+import java.io.*;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class forloop {
@@ -24,3 +26,381 @@ public class forloop {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package FWSA;
+        import java.util.*;
+        import java.lang.*;
+        import java.io.*;
+
+class user implements Serializable {
+    public HashMap<Integer,String> hp=new HashMap<>();
+}
+
+public class test {
+    public static void main(String[] args) throws FileNotFoundException {
+        FWSA.user name = new FWSA.user();
+        name.hp.put(1,"chith");
+        name.hp.put(2,"kasi");
+        try {
+            FileOutputStream fpo = new FileOutputStream("D:\\chith.txt");
+            ObjectOutputStream oop = new ObjectOutputStream(fpo);
+            oop.writeObject(name);
+            oop.close();
+            fpo.close();
+            System.out.println("finished");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//reverse array
+
+
+// Reversing an array using Java collections
+import java.util.*;
+
+public class reversingArray {
+
+    // function reverses the elements of the array
+    static void reverse(Integer a[])
+    {
+        Collections.reverse(Arrays.asList(a));
+        System.out.println(Arrays.asList(a));
+    }
+
+    public static void main(String[] args)
+    {
+        Integer [] arr = {10, 20, 30, 40, 50};
+        reverse(arr);
+    }
+}
+
+
