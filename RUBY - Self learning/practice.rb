1da72cf9e9e46788070c79c9995305dfc068eabc
Chithraj <chithraj23102003@gmail.com>
2022-06-16 11:47:37 +0530

    wd201 starting

---
+++ RUBY - Self learning/practice.rb
@@ -0,0 +1,8 @@
+print "What's your first name?"
+
+first_name = gets.chomp
+last_name = gets.chomp
+city = gets.chomp
+print "#{first_name}"
+print "#{last_name}"
+print "#{city}".upcase
---
+++ WD201-Ruby on rails/irb.rb
@@ -0,0 +1,7 @@
+#creating and assuming variable and array
+
+mobile = ["redmi","realmi","Apple","Samsung"]
+
+puts mobile.length
+
+push.mobile["lava"]
